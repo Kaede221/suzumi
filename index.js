f11ed1e7c70b14eba8ff17468ddf72ca84ee4ab4
Kaede <kaedeshimizu@outlook.com>
2024-02-08 19:37:00 +0800

    修改代码逻辑
    存在错误的逻辑，修改一下

--- index.js
+++ index.js
@@ -26,17 +26,17 @@ app.get('/json', (req, res) => {
 
 // 以及直接访问 给两个方案就好
 app.all('/', (req, res) => {
+    // 正常获取需要的参数
+    const query = req.query
+    const r18 = query.r18 || config.app.r18
+    const proxy = query.proxy || config.app.proxy
+    const tag = query.proxy || true
+    const num = query.num || config.app.num
+
+    // 根据具体情况进行判断
     if (req.method == "GET") {
-        const query = req.query
-        const r18 = query.r18 || config.app.r18
-        const proxy = query.proxy || config.app.proxy
-        const tag = query.proxy || true
         res.redirect(kaede.getRandLink(jsonData, proxy, r18, tag))
     } else if (req.method == "POST") {
-        const query = req.query
-        const r18 = query.r18 || config.app.r18
-        const tag = query.proxy || true
-        const num = query.num || config.app.num
         res.json(kaede.getRandJson(jsonData, r18, tag, num))
     }
 })
