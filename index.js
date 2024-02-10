3c09bbc4cc8adf7e1aa611cc11f91b9b08f75e32
Kaede <kaedeshimizu@outlook.com>
2024-02-10 20:39:31 +0800

    添加筛选器，解决一些Bug问题

--- index.js
+++ index.js
@@ -9,35 +9,41 @@ const jsonData = require(`./data/${config.app.db}`)
 // 直接查看访问的参数就好
 app.get('/direct', (req, res) => {
     const query = req.query
-    const r18 = query.r18 || config.app.r18
+    const r18 = query.r18
     const proxy = query.proxy || config.app.proxy
-    const tag = query.tag || true
-    res.redirect(kaede.getRandLink(jsonData, proxy, r18, tag))
+    const tag = query.tag
+    const uid = query.uid
+    const keyword = query.keyword
+    res.redirect(kaede.getRandLink(jsonData, proxy, r18, tag, uid, keyword))
 })
 
 // JSON情况
 app.get('/json', (req, res) => {
     const query = req.query
-    const r18 = query.r18 || config.app.r18
-    const tag = query.tag || true
+    const r18 = query.r18
+    const tag = query.tag
     const num = query.num || config.app.num
-    res.json(kaede.getRandJson(jsonData, r18, tag, num))
+    const uid = query.uid
+    const keyword = query.keyword
+    res.json(kaede.getRandJson(jsonData, r18, tag, num, uid, keyword))
 })
 
 // 以及直接访问 给两个方案就好
 app.all('/', (req, res) => {
     // 正常获取需要的参数
     const query = req.query
-    const r18 = query.r18 || config.app.r18
+    const r18 = query.r18
     const proxy = query.proxy || config.app.proxy
-    const tag = query.tag || true
+    const tag = query.tag
     const num = query.num || config.app.num
+    const uid = query.uid
+    const keyword = query.keyword
 
     // 根据具体情况进行判断
     if (req.method == "GET") {
-        res.redirect(kaede.getRandLink(jsonData, proxy, r18, tag))
+        res.redirect(kaede.getRandLink(jsonData, proxy, r18, tag, uid, keyword))
     } else if (req.method == "POST") {
-        res.json(kaede.getRandJson(jsonData, r18, tag, num))
+        res.json(kaede.getRandJson(jsonData, r18, tag, num, uid, keyword))
     }
 })
 
--- utils/kaede.js
+++ utils/kaede.js
@@ -15,26 +15,51 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // 不包含最大值，包含最小值
 }
 
+// 写一个筛选器出来
+// 传入一个K-Data，根据规则进行筛选，返回视情况而定
+// 默认参数
+// - r18 int 是否为R18内容，默认使用0，防止传入错误参数
+// - tag string 是否有Tag，如果没有的话默认就是null，否则就是一个内容
+// - uid string 根据具体作者进行筛选
+// - keyword string 可以查找作品名称中以及tags中的关键字
+function dataFilter(kData, r18 = 0, tag = null, uid = null, keyword = null) {
+    // 为了看起来方便，我还是用连续的if吧
+    if (kData['r18'] == r18) {
+        if (tag == null || kData['tags'].includes(tag)) {
+            if (uid == null || parseInt(kData['uid']) == uid) {
+                if (keyword == null || kData['tags'].includes(keyword) || kData['title'].includes(keyword)) {
+                    return kData
+                }
+            }
+        }
+    }
+    return false
+}
+
 // 获取一个随机的链接
-function getRandLink(jsonData, proxy, r18, tag) {
+function getRandLink(jsonData, proxy, r18, tag, uid, keyword) {
     // 创建空列表，用来储存获取的所有链接
     let urlList = []
 
     // 判断r18和tags，进行筛选，获得链接
     jsonData.forEach(element => {
-        if ((element['r18'] == r18) || r18 == 2 && tag || element['tags'].includes(tag)) {
+        if (dataFilter(element, r18, tag, uid, keyword)) {
             // 直接加入列表就好
             urlList.push(element['url'])
         }
     });
+    // 这里判断一下，是否有链接，如果没有，就是一个空列表，那么返回一个404页面就好
+    if (urlList.length == 0) {
+        return '404.html'
+    }
     // 根据列表长短，获取一个随机的链接
     return useProxy(proxy, urlList[getRandomInt(0, urlList.length)])
 }
 
 // 获取随机的JSON文件
-function getRandJson(jsonData, r18, tag, num) {
+function getRandJson(jsonData, r18, tag, num, uid, keyword) {
     // 首先判断num的范围是否成立
-    if (num < 1 || num > 10) {
+    if (!(num >= 1 && num <= 20)) {
         // 如果超出范围，那么返回空数组
         return []
     }
@@ -43,12 +68,11 @@ function getRandJson(jsonData, r18, tag, num) {
 
     // 先进行筛选
     jsonData.forEach(element => {
-        if ((element['r18'] == r18) || r18 == 2 && tag || element['tags'].includes(tag)) {
+        if (dataFilter(element, r18, tag, uid, keyword)) {
             // 直接加入列表就好
             objectList.push(element)
         }
     })
-    // 筛选后，直接随机num个对象加入就好
     let finalList = []
     let l = objectList.length
     for (let i = 0; i < num; i++) {
